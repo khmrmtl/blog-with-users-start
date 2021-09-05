c09bbb87befba974cd6e0edcfc61ebed831b47ad
merk <motalkhmer@gmail.com>
2021-09-05 13:00:38 +0800

    Add gunicorn pilit ulit last

--- forms.py
+++ forms.py
@@ -23,7 +23,7 @@ class RegisterForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = EmailField("Email", validators=[DataRequired(), Email()])
+    email = EmailField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
